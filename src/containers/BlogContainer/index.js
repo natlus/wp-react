383b6defcecbdf5a62b42f3581b68df9ecf1a534
Jesper Sultan <jesper.sultan@gmail.com>
2017-01-04 20:49:58 +0000

    Formatting

--- src/containers/BlogContainer/index.js
+++ src/containers/BlogContainer/index.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Index from '../../containers/Index';
 
 class BlogContainer extends Component {
-
   render() {
     return !this.props.params.slug
     ? <Index routes={this.props.routes} />
