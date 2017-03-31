12791a9ab7910beca2d9883e7c7050c36c31886c
Jesper Sultan <jesper.sultan@prisjakt.nu>
2017-03-31 14:13:52 +0200

    Refactor breadcrumbs

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -3,25 +3,19 @@ import { Link } from 'react-router';
 
 import { Bread, Crumb } from './styles';
 
-const Breadcrumbs = ({ routes }) => {
-  const crumbs = routes.map((route, index) => (
-    <Crumb key={index} last={(index + 1) === routes.length}>
-      {(index + 1) !== routes.length ? (
-        <Link to={route.path}>
-          { route.component.title || route.name }
-        </Link>
-      ) : (
-        route.component.title || route.name
-      )}
-    </Crumb>
-  ));
-
-  return (
-    <Bread>
-      {crumbs}
-    </Bread>
-  )
-}
+const Breadcrumbs = ({ routes }) => (
+  <Bread>
+    {routes.map((route, index) => (
+      <Crumb key={index} last={index + 1 === routes.length}>
+        {index + 1 !== routes.length
+          ? <Link to={route.path}>
+              {route.component.title || route.name}
+            </Link>
+          : route.component.title || route.name}
+      </Crumb>
+    ))}
+  </Bread>
+);
 
 Breadcrumbs.propTypes = {
   routes: React.PropTypes.array.isRequired,
