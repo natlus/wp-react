30ab4239aaa160452e1177cdc2352d9c22f7082e
Jesper Sultan <jesper.sultan@prisjakt.nu>
2017-03-31 15:04:20 +0200

    Refactor BlogPost

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -15,46 +15,40 @@ import {
 } from './styles.js';
 
 // dangerouslySetInnerHTML
-const createMarkup = (html) => ({ __html: html });
+const createMarkup = html => ({ __html: html });
 
 const BlogPost = ({ title, content, date, slug = '', categories, tags }) => {
   const timeagoInstance = new timeago();
 
-  const categoriesMarkup = categories.length > 0 && (
+  const categoriesMarkup = categories.length > 0 &&
     <Categories>
-      { categories.map((category, index) => (
-          <Category to={`/blog/category/${category.slug}`} key={index}>
-            {category.name}
-          </Category>
-        ))
-      }
-    </Categories>
-  );
+      {categories.map((category, index) => (
+        <Category to={`/blog/category/${category.slug}`} key={index}>
+          {category.name}
+        </Category>
+      ))}
+    </Categories>;
 
-  const tagMarkup = tags.length > 0 && (
+  const tagMarkup = tags.length > 0 &&
     <Tags>
-      { tags.map((tag, index) => (
-          <Tag key={index} to={`/blog/tag/${tag.slug}`}>
-            {tag.name}
-          </Tag>
-        ))
-      }
-    </Tags>
-  );
+      {tags.map((tag, index) => (
+        <Tag key={index} to={`/blog/tag/${tag.slug}`}>
+          {tag.name}
+        </Tag>
+      ))}
+    </Tags>;
 
   const titleMarkup = (
     <Title link={slug !== ''}>
-      {slug !== '' ? (
-        <h2>
-          <Link to={`/blog/post/${slug}`}>
-            {title}
-          </Link>
-        </h2>
-      ) : (
-        <h1>{title}</h1>
-      )}
+      {slug !== ''
+        ? <h2>
+            <Link to={`/blog/post/${slug}`}>
+              {title}
+            </Link>
+          </h2>
+        : <h1>{title}</h1>}
     </Title>
-  )
+  );
 
   return (
     <Wrapper>
@@ -67,10 +61,10 @@ const BlogPost = ({ title, content, date, slug = '', categories, tags }) => {
           {categoriesMarkup}
         </Metadata>
       </Header>
-      <Content dangerouslySetInnerHTML={ createMarkup(content) } />
+      <Content dangerouslySetInnerHTML={createMarkup(content)} />
       {tagMarkup}
     </Wrapper>
-  )
+  );
 };
 
 BlogPost.propTypes = {
